e05e15d386999a9dda65acbd4be7d921a92d48fc
Chandan Kumar <chandankrr.91@gmail.com>
2024-12-07 00:12:33 +0530

    feat: design hero section

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,3 +1,39 @@
+import Image from "next/image";
+
+import { Button } from "@/components/button";
+import { Heading } from "@/components/heading";
+import chatImage from "@/public/chat.png";
+
 export default function Home() {
-  return <div>Slide</div>;
+  return (
+    <>
+      {/* Hero Section */}
+      <section className="mt-16 md:mt-20 lg:mt-32">
+        <div className="container text-center">
+          <div>
+            <Heading className="mt-24 md:mt-28 lg:mt-0">
+              Unleash the power of chat marketing
+            </Heading>
+
+            <p className="mx-auto mt-5 max-w-xl text-xl leading-6 md:max-w-lg lg:max-w-xl">
+              Harness the power of AI to boost engagement, grow your audience,
+              and save countless hours. Experience seamless comments, DMs, and
+              more.
+            </p>
+
+            <Button variant="primary" className="mt-5 tracking-widest">
+              Get Started For Free
+            </Button>
+          </div>
+          <div className="mt-12 text-center">
+            <Image
+              src={chatImage}
+              className="mx-auto select-none pointer-events-none size-[95%] md:size-[75%] lg:size-[60%]"
+              alt="Image of product demo"
+            />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
---
+++ components/heading.tsx
@@ -0,0 +1,26 @@
+import { HTMLAttributes } from "react";
+
+import { cn } from "@/lib/utils";
+
+interface HeadingProps extends HTMLAttributes<HTMLHeadingElement> {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export const Heading = ({
+  children,
+  className,
+  ...otherProps
+}: HeadingProps) => {
+  return (
+    <h2
+      className={cn(
+        "mx-auto max-w-xl text-4xl leading-8 font-heading md:text-7xl lg:text-7xl text-pretty md:max-w-2xl lg:max-w-3xl",
+        className
+      )}
+      {...otherProps}
+    >
+      {children}
+    </h2>
+  );
+};
